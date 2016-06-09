010ce3d6c183913b916be7fa4e23798ac6ff27aa
Steven McConomy <srmcconomy@gmail.com>
2016-06-09 15:10:23 -0400

    a

--- A3/A3.cpp
+++ A3/A3.cpp
@@ -424,7 +424,7 @@ void A3::renderSceneGraph(const SceneNode & root) {
 
 		// Get the BatchInfo corresponding to the GeometryNode's unique MeshId.
 		BatchInfo batchInfo = m_batchInfoMap[geometryNode->meshId];
-    cout << batchInfo.numIndices << endl;
+    cout << geometryNode->m_name << endl;
 		//-- Now render the mesh:
 		m_shader.enable();
 		glDrawArrays(GL_TRIANGLES, batchInfo.startIndex, batchInfo.numIndices);
