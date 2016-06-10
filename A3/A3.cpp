23c097be6004e529c5ac2089a624628e8ddbe7b3
Steven McConomy <srmcconomy@gmail.com>
2016-06-10 16:56:47 -0400

    a

--- A3/A3.cpp
+++ A3/A3.cpp
@@ -525,7 +525,7 @@ bool A3::mouseMoveEvent (
   }
 
   if (mouseRightDown) {
-    float angle = acos(dot(trackball, lastTrackball));
+    float angle = acos(clamp(dot(trackball, lastTrackball), 0, 1.0f);
     cout << trackball << ',' << lastTrackball << endl;
     cout << dot(trackball, lastTrackball) << ',' << angle << endl;
     rotationTrans = rotate(mat4(1.0f), angle, cross(trackball, lastTrackball)) * rotationTrans;
