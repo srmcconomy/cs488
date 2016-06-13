b6ffa0e9a98fc1ced8644632e028e40f872d3e6a
Steven McConomy <srmcconomy@gmail.com>
2016-06-13 10:53:57 -0400

    a

--- A3/JointNode.cpp
+++ A3/JointNode.cpp
@@ -1,4 +1,6 @@
 #include "JointNode.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
 using namespace glm;
 //---------------------------------------------------------------------------------------
 JointNode::JointNode(const std::string& name)
