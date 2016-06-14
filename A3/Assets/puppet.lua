409983ccd0f8e331f1858020f889016024f577ef
Steven McConomy <srmcconomy@gmail.com>
2016-06-14 16:24:40 -0400

    a

--- A3/Assets/puppet.lua
+++ A3/Assets/puppet.lua
@@ -135,7 +135,7 @@ leftHipNode:add_child(leftHipJoint)
 
 leftThigh = gr.mesh('cube', 'leftThigh')
 leftHipNode:add_child(leftThigh)
-leftThigh:translate(0, 0.5, 0)
+leftThigh:translate(0, -0.5, 0)
 leftThigh:scale(0.1, 0.5, 0.1)
 leftThigh:set_material(red)
 
