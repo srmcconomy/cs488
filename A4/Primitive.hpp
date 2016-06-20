cdfeea5b0ccb2b80bf3038b6813ef9b0ef4312b1
Steven McConomy <srmcconomy@gmail.com>
2016-06-20 15:54:00 -0400

    a

--- A4/Primitive.hpp
+++ A4/Primitive.hpp
@@ -66,12 +66,12 @@ public:
     bool isect = false;
     for (uint i = 0; i < 6; i++) {
       vec3 n = ns[i];
-      vec3 p = m_pos + n;
+      vec3 p = m_pos + vec3(m_size / 2.0f, m_size / 2.0f, m_size / 2.0f) + n;
       float d = dot(p - eye, n) / dot(ray, n);
       point = eye + ray * d;
-      if ((point - p).x > 0 && (point - p).x < (float)m_size
-        && (point - p).y > 0 && (point - p).y < (float)m_size
-        && (point - p).z > 0 && (point - p).z < (float)m_size) {
+      if ((point - p).x > m_size / 2.0f && (point - p).x < m_size / 2.0f
+        && (point - p).y > m_size / 2.0f && (point - p).y < m_size / 2.0f
+        && (point - p).z > m_size / 2.0f && (point - p).z < m_size / 2.0f) {
         return i + 1;
       }
     }
