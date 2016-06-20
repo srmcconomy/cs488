03a681e7befb7a7a828b1601d0ff1fdfc28ba201
Steven McConomy <srmcconomy@gmail.com>
2016-06-20 14:33:44 -0400

    a

--- A4/A4.cpp
+++ A4/A4.cpp
@@ -59,7 +59,7 @@ void A4_Render(
       image(x, y, 2) = 0;
       double angley = (y - h / 2) / h * fovy;
       double anglex = (x - w / 2) / w * fovx;
-      rotate(mainRay, anglex, up);
+      rotate(mainRay, (float)anglex, up);
       //mat4 rotation = rotate(mat4(1.0), anglex, up4);
       //rotation = rotate(rotation, angley, cross(up4, mainRay4));
       vec4 ray4 = mainRay4;
