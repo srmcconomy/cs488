a4aec47e25f7f180143355c42caba67ced391a31
Steven McConomy <srmcconomy@gmail.com>
2016-06-20 15:12:05 -0400

    a

--- A4/Primitive.hpp
+++ A4/Primitive.hpp
@@ -57,7 +57,7 @@ public:
   size_t intersect(const glm::vec3& eye, const glm::vec3& ray, glm::vec3& point, glm::vec3& normal) {
     vec3 n = vec3((float)m_size, 0, 0);
     vec3 p = m_pos + n;
-    double d = dot(p - eye, n) / dot(ray, n);
+    float d = dot(p - eye, n) / dot(ray, n);
     point = eye + ray * d;
     if ((point - p).x > -m_size && (point - p).x < m_size) {
       return 1;
