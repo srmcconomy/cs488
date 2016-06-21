cbb4f72f37221f82966668efeb6461113be744f1
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 12:32:48 -0400

    a

--- A4/Primitive.hpp
+++ A4/Primitive.hpp
@@ -80,17 +80,24 @@ public:
       vec3(-(float)m_size / 2.0f, 0, 0)
     };
     bool isect = false;
+    float mind = -1.0f;
     for (uint i = 0; i < 6; i++) {
       vec3 n = ns[i];
       vec3 p = m_pos + vec3(m_size / 2.0f, m_size / 2.0f, m_size / 2.0f) + n;
       float d = dot(p - eye, n) / dot(ray, n);
-      point = eye + ray * d;
-      if ((point - p).x > -m_size / 2.0f && (point - p).x < m_size / 2.0f
-        && (point - p).y > -m_size / 2.0f && (point - p).y < m_size / 2.0f
-        && (point - p).z > -m_size / 2.0f && (point - p).z < m_size / 2.0f) {
-        return i + 1;
+      if (d > 0 && (mind < 0 || d < mind)) {
+        vec3 pt = eye + ray * d;
+        if ((pt - p).x > -m_size / 2.0f && (pt - p).x < m_size / 2.0f
+          && (pt - p).y > -m_size / 2.0f && (pt - p).y < m_size / 2.0f
+          && (pt - p).z > -m_size / 2.0f && (pt - p).z < m_size / 2.0f) {
+            mind = d;
+            point = pt;
+            normal = n;
+            isect = true;
+        }
       }
     }
+    normal = normalize(normal);
     return isect ? 1 : 0;
   }
 
