029cabf53b59a66d6f0325b6fdf0163879357eb1
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 14:13:54 -0400

    a

--- A4/Mesh.cpp
+++ A4/Mesh.cpp
@@ -55,6 +55,7 @@ bool Mesh::intersect(const vec3& ray, const vec3& eye, vec3& point, vec3& normal
 		vec3 n = normalize(cross(u, v));
 		float d2 = dot(m_vertices[face.v2] - eye, n) / dot(ray, n);
 		if (!isect || (d * d2 > 0 && abs(d2) < abs(d))) {
+			std::cout << d2 << std::endl;
 			vec3 pt = eye + ray * d2;
 			vec3 w = pt - m_vertices[face.v2];
 
@@ -78,6 +79,5 @@ bool Mesh::intersect(const vec3& ray, const vec3& eye, vec3& point, vec3& normal
 			isect = true;
 		}
 	}
-	std::cout << isect << std::endl;
 	return isect;
 }
