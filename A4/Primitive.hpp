05dd678efc7792578034f34463db4405f3486f8c
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 16:53:25 -0400

    a

--- A4/Primitive.hpp
+++ A4/Primitive.hpp
@@ -20,11 +20,15 @@ public:
 class Sphere : public Primitive {
 public:
   virtual ~Sphere();
+  bool intersect(const glm::vec3& eye, const glm::vec3& ray, glm::vec3& point, glm::vec3& normal, float& d);
+  bool intersect(const glm::vec3& eye, const glm::vec3& ray, const glm::mat4 transform, glm::vec3& point, glm::vec3& normal, float& d);
 };
 
 class Cube : public Primitive {
 public:
   virtual ~Cube();
+  bool intersect(const glm::vec3& eye, const glm::vec3& ray, glm::vec3& point, glm::vec3& normal, float& d);
+  bool intersect(const glm::vec3& eye, const glm::vec3& ray, const glm::mat4 transform, glm::vec3& point, glm::vec3& normal, float& d);
 };
 
 class NonhierSphere : public Primitive {
@@ -34,38 +38,8 @@ public:
   {
   }
   virtual ~NonhierSphere();
-  bool intersect(const glm::vec3& eye, const glm::vec3& ray, glm::vec3& point, glm::vec3& normal, float& d) {
-    double roots[2];
-    size_t i = quadraticRoots(dot(ray, ray),
-      2 * dot(ray, eye - m_pos),
-      dot(eye - m_pos, eye - m_pos) - m_radius * m_radius,
-      roots);
-    switch(i) {
-      case 0:
-        return false;
-      case 1:
-        point = eye + (float)roots[0] * ray;
-        normal = point - m_pos;
-        d = (float)roots[0];
-        return true;
-      case 2:
-        double root;
-        if (roots[0] < 0 && roots[1] < 0) {
-          root = roots[0] < roots[1] ? roots[1] : roots[0];
-        } else if (roots[0] < 0) {
-          root = roots[1];
-        } else if (roots[1] < 0) {
-          root = roots[0];
-        } else {
-          root = roots[0] > roots[1] ? roots[1] : roots[0];
-        }
-        point = eye + (float)root * ray;
-        normal = normalize(point - m_pos);
-        d = (float)root;
-        return true;
-    }
-  }
-
+  bool intersect(const glm::vec3& eye, const glm::vec3& ray, glm::vec3& point, glm::vec3& normal, float& d);
+  bool intersect(const glm::vec3& eye, const glm::vec3& ray, const glm::mat4 transform, glm::vec3& point, glm::vec3& normal, float& d);
 private:
 
   glm::vec3 m_pos;
