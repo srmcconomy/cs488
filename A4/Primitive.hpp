59627b26c60e0359dc8591922967fbf960c0a398
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 12:55:49 -0400

    a

--- A4/Primitive.hpp
+++ A4/Primitive.hpp
@@ -47,7 +47,9 @@ public:
         break;
       case 2:
         double root;
-        if (roots[0] < 0) {
+        if (roots[0] < 0 && roots[1] < 0) {
+          root = roots[0] < roots[1] ? roots[1] : roots[0];
+        } else if (roots[0] < 0) {
           root = roots[1];
         } else if (roots[1] < 0) {
           root = roots[0];
