f07f58611f9276d44a850375129e70ea7dd09323
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 14:54:20 -0400

    a

--- A4/Mesh.cpp
+++ A4/Mesh.cpp
@@ -54,7 +54,7 @@ bool Mesh::intersect(const vec3& ray, const vec3& eye, vec3& point, vec3& normal
 		vec3 v = m_vertices[face.v3] - m_vertices[face.v2];
 		vec3 n = normalize(cross(u, v));
 		float nray = dot(ray, n);
-		if (nray <= 0) continue;
+		if (nray >= 0) continue;
 		float d2 = dot(m_vertices[face.v2] - eye, n) / nray;
 		if (d2 > 0 && (!isect || d2 < d)) {
 			vec3 pt = eye + ray * d2;
