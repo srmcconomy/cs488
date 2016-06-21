a5e697b8cc83e3bccabbb6721385a73366384fe0
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 19:41:44 -0400

    a

--- A4/Assets/hier.lua
+++ A4/Assets/hier.lua
@@ -37,6 +37,7 @@ plane = gr.mesh( 'plane', 'Assets/plane.obj' )
 scene:add_child(plane)
 plane:set_material(grass)
 plane:scale(30, 30, 30)
+plane:rotate(180, 0, 0)
 
 -- sphere
 poly = gr.mesh( 'poly', 'Assets/dodeca.obj' )
