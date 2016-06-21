17a60273511de39eda68e580e4e7984b73dd8d37
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 15:15:04 -0400

    a

--- A4/A4.cpp
+++ A4/A4.cpp
@@ -86,6 +86,7 @@ void A4_Render(
 							vec3 normal2;
 							float dNode;
 							geonode->m_primitive->intersect(light->position, l, point2, normal2, dNode);
+              cout <<to_string(point) << to_string(point2) << endl;
               vec3 distance = point2 - point;
               if (abs(dot(distance, distance)) > EPSILON) {
                 lightHits = false;
