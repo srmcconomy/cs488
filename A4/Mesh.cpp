8ebdc4bc7a76dfd304da3d1d2b1ebe020cdee3f8
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 17:33:42 -0400

    a

--- A4/Mesh.cpp
+++ A4/Mesh.cpp
@@ -73,6 +73,7 @@ bool Mesh::intersect(const vec3& eye, const vec3& ray, vec3& point, vec3& normal
 		float nray = dot(ray, n);
 		if (nray >= 0) continue;
 		float d2 = dot(m_vertices[face.v2] - eye, n) / nray;
+    std::cout << d2 << std::endl;
 		if (d2 > 0 && (!isect || d2 < d)) {
 			vec3 pt = eye + ray * d2;
 			vec3 w = pt - m_vertices[face.v2];
