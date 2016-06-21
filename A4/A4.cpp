0b9706562bdda28654d375edfbb3224b2a1a5a38
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 15:27:10 -0400

    a

--- A4/A4.cpp
+++ A4/A4.cpp
@@ -105,15 +105,16 @@ void A4_Render(
   							}
               }
 							if (lightHits) {
+                float attenuation = 1.0f / (falloff[0] + dNode * falloff[1] + dNode * dNode * falloff[2]);
 								vec3 r = reflect(l, normal);
 								for (int c = 0; c < 3; c++) {
 									float dotp = dot(-l, normal);
 									if (dotp < 0) dotp = 0;
-									colour[c] += phong->m_kd[c] * dotp * light->colour[c];
+									colour[c] += phong->m_kd[c] * dotp * light->colour[c] / attenuation;
 
 									dotp = dot(r, -ray);
 									if (dotp < 0) dotp = 0;
-									colour[c] += phong->m_ks[c] * pow(dotp, phong->m_shininess) * light->colour[c];
+									colour[c] += phong->m_ks[c] * pow(dotp, phong->m_shininess) * light->colour[c] / attenuation;
 								}
 							}
 
