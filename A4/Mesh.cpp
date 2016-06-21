b143cfec54a1d71a99a34da9f202b84572030f84
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 17:10:23 -0400

    a

--- A4/Mesh.cpp
+++ A4/Mesh.cpp
@@ -53,6 +53,7 @@ bool Mesh::intersect(const vec3& eye, const vec3& ray, const mat4& transform, ve
   vec4 ray4(eye.x, eye.y, eye.z, 0);
   eye4 = inv * eye4;
   ray4 = inv * ray4;
+	std::cout << to_string(eye) << " => " << to_string(eye4) << std::endl;
   bool ret = intersect(vec3(eye4.y, eye4.x, eye4.z), vec3(ray4.x, ray4.y, ray4.z), point, normal, d);
   vec4 point4 = transform * vec4(point.x, point.y, point.z, 1.0f);
   vec4 normal4 = transform * vec4(normal.x, normal.y, normal.z, 0);
