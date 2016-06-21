f5702a73a1d8a1a8991020a4f1fc448cc1ea46a4
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 13:58:41 -0400

    a

--- A4/Mesh.cpp
+++ A4/Mesh.cpp
@@ -49,6 +49,7 @@ std::ostream& operator<<(std::ostream& out, const Mesh& mesh)
 
 bool Mesh::intersect(const vec3& ray, const vec3& eye, vec3& point, vec3& normal, float& d) {
 	bool isect = false;
+		std::cout << "HI" << std::endl;
 	for (Triangle face : m_faces) {
 		std::cout << "HI" << std::endl;
 		vec3 u = m_vertices[face.v1] - m_vertices[face.v2];
