c1b48221d49b63f0540349498df08a316e11f25f
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 16:56:28 -0400

    a

--- A4/Primitive.cpp
+++ A4/Primitive.cpp
@@ -15,8 +15,10 @@ bool Sphere::intersect(const vec3& eye, const vec3& ray, const mat4& transform, 
   eye4 = inv * eye4;
   ray4 = inv * ray4;
   bool ret = intersect(vec3(eye4.y, eye4.x, eye4.z), vec3(ray4.x, ray4.y, ray4.z), point, normal, d);
-  point = transform * point;
-  normal = transform * normal;
+  vec4 point4 = transform * vec4(point.x, point.y, point.z, 1.0f);
+  vec4 normal4 = transform * vec4(normal.x, normal.y, normal.z, 0);
+  point = vec3(point4.x, point4.y, point4.z);
+  normal = vec3(normal4.x, normal4.y, normal4.z);
   d = length(point - eye);
   return ret;
 }
