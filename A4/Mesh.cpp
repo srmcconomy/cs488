7bc99b6db6f0248ff447c57b08a8ef54e9fa89a9
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 17:28:10 -0400

    a

--- A4/Mesh.cpp
+++ A4/Mesh.cpp
@@ -64,8 +64,10 @@ bool Mesh::intersect(const vec3& eye, const vec3& ray, const mat4& transform, ve
 }
 
 bool Mesh::intersect(const vec3& eye, const vec3& ray, vec3& point, vec3& normal, float& d) {
+  std::cout << "H" << std::endl;
   bool isect = false;
 	for (Triangle face : m_faces) {
+    std::cout << "T" << std::endl;
 		vec3 u = m_vertices[face.v1] - m_vertices[face.v2];
 		vec3 v = m_vertices[face.v3] - m_vertices[face.v2];
 		vec3 n = -normalize(cross(u, v));
