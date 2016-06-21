1574b9d377e7eef9be87845e41e4ffc6ff71b19e
Steven McConomy <srmcconomy@gmail.com>
2016-06-21 14:11:20 -0400

    a

--- A4/Assets/nonhier.lua
+++ A4/Assets/nonhier.lua
@@ -7,29 +7,29 @@ mat4 = gr.material({0.7, 0.6, 1.0}, {0.5, 0.4, 0.8}, 25)
 
 scene_root = gr.node('root')
 
-s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
-scene_root:add_child(s1)
-s1:set_material(mat1)
-
-s2 = gr.nh_sphere('s2', {200, 50, -100}, 150)
-scene_root:add_child(s2)
-s2:set_material(mat1)
-
-s3 = gr.nh_sphere('s3', {0, -1200, -500}, 1000)
-scene_root:add_child(s3)
-s3:set_material(mat2)
-
-b1 = gr.nh_box('b1', {-200, -125, 0}, 100)
-scene_root:add_child(b1)
-b1:set_material(mat4)
-
-s4 = gr.nh_sphere('s4', {-100, 25, -300}, 50)
-scene_root:add_child(s4)
-s4:set_material(mat3)
-
-s5 = gr.nh_sphere('s5', {0, 100, -250}, 25)
-scene_root:add_child(s5)
-s5:set_material(mat1)
+-- s1 = gr.nh_sphere('s1', {0, 0, -400}, 100)
+-- scene_root:add_child(s1)
+-- s1:set_material(mat1)
+--
+-- s2 = gr.nh_sphere('s2', {200, 50, -100}, 150)
+-- scene_root:add_child(s2)
+-- s2:set_material(mat1)
+--
+-- s3 = gr.nh_sphere('s3', {0, -1200, -500}, 1000)
+-- scene_root:add_child(s3)
+-- s3:set_material(mat2)
+--
+-- b1 = gr.nh_box('b1', {-200, -125, 0}, 100)
+-- scene_root:add_child(b1)
+-- b1:set_material(mat4)
+--
+-- s4 = gr.nh_sphere('s4', {-100, 25, -300}, 50)
+-- scene_root:add_child(s4)
+-- s4:set_material(mat3)
+--
+-- s5 = gr.nh_sphere('s5', {0, 100, -250}, 25)
+-- scene_root:add_child(s5)
+-- s5:set_material(mat1)
 
 -- A small stellated dodecahedron.
 
